7962def59f457dba5536a86dde3a82a96b672f3d
HSamiul <samiulh@andrew.cmu.edu>
2024-08-04 22:04:53 -0400

    add AccessTokenResponse struct

---
+++ Sources/power-library/Models/Responses/AccessTokenResponse.swift
@@ -0,0 +1,29 @@
+//
+//  AccessTokenResponse.swift
+//
+//
+//  Created by Samiul Hoque on 8/4/24.
+//
+
+import Foundation
+
+/// Response received from Spotify's `api/token` endpoint.
+internal struct AccessTokenResponse: Decodable {
+    
+    let accessToken: String
+    let tokenType: String
+    let expiresIn: Int
+    let refreshToken: String?
+    let scope: String
+}
+
+private extension AccessTokenResponse {
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case expiresIn = "expires_in"
+        case refreshToken = "refresh_token"
+        case scope = "scope"
+    }
+}
