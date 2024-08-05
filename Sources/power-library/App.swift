27396e3af95bfc9e40dd7a407dea0cf1d6de8ae7
HSamiul <samiulh@andrew.cmu.edu>
2024-08-04 21:18:30 -0400

    changed SpotifyApp to App

--- Sources/power-library/App.swift
+++ Sources/power-library/App.swift
@@ -1,14 +1,14 @@
 //
-//  File.swift
-//  
+//  App.swift
+//
 //
 //  Created by Samiul Hoque on 8/4/24.
 //
 
 import Foundation
 
-/// The details of your Spotify app needed to authorize it.
-public struct SpotifyApp {
+/// Your Spotify app.
+public struct App {
     
     /// The client ID of your Spotify app.
     private let clientId: String
