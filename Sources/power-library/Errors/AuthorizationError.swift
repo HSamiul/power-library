036881837476f01873cfdffaa1ce894269250763
HSamiul <samiulh@andrew.cmu.edu>
2024-08-05 20:26:19 -0400

    added AuthorizationError enum

--- Sources/power-library/Errors/AuthorizationError.swift
+++ Sources/power-library/Errors/AuthorizationError.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
-//  
+//  AuthorizationError.swift
+//
 //
 //  Created by Samiul Hoque on 8/5/24.
 //
 
 import Foundation
+
+/// Errors that may be thrown by `Authorization` .
+enum AuthorizationError: LocalizedError {
+    
+    /// An access token was attemped to be refreshed, but there is no refresh token.
+    case RefreshTokenMissing
+    
+    var errorDescription: String? {
+        switch self {
+        case .RefreshTokenMissing:
+            return "An access token was attemped to be refreshed, but there is no refresh token."
+        }
+    }
+}
