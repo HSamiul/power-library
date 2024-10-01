ebec84c86e6641f993ea97920c78d9885d7ad4fa
HSamiul <samiulh@andrew.cmu.edu>
2024-10-01 19:50:35 -0400

    add api and service layer Spotify search

--- Sources/power-library/PowerSearchService.swift
+++ Sources/power-library/PowerSearchService.swift
@@ -5,30 +5,35 @@
 //  Created by Samiul Hoque on 9/30/24.
 //
 
+import Foundation
+
+@available(macOS 13.0, *)
 public final class PowerSearchService {
     
     private var authService: PowerAuthService
     
-    public init(authService: PowerAuthService) {
+    public init(_ authService: PowerAuthService) {
         self.authService = authService
     }
 }
 
 
+@available(macOS 13.0, *)
 public extension PowerSearchService {
     
     func search(query: String) async throws -> [Track] {
-        let response = SpotifySearchApi.search(
+        let response = try await SpotifySearchApi.requestSearch(
             query: query,
             accessToken: authService.accessToken!
         )
         
-        let apiTracks = return response.tracks.items
+        let apiTracks = response.tracks.items
         
-        let tracks = items.map { apiTrack in
+        let tracks = apiTracks.map { apiTrack in
             Track(
                 trackName: apiTrack.name,
-                artistName: apiTrack.artists.first!.name
+                artistName: apiTrack.artists.first!.name,
+                imageUrl: URL(string: apiTrack.album.images.last!.url)!
             )
         }
         
--- Sources/power-library/SpotifySearchApi.swift
+++ Sources/power-library/SpotifySearchApi.swift
@@ -5,10 +5,55 @@
 //  Created by Samiul Hoque on 9/30/24.
 //
 
+import Foundation
+
 /// Sends HTTP requests to the Spotify API's search endpoints.
+@available(macOS 13.0, *)
+@available(iOS 16.0, *)
 public class SpotifySearchApi {
     
-//    static func search(query: String) async throws -> SpotifyApiSearchResponse {
-//        
-//    }
+    public static func requestSearch(
+        query: String,
+        accessToken: String
+    ) async throws -> SpotifyApiSearchResponse {
+        
+        let queryItems = [
+            URLQueryItem(name: "q", value: query),
+            URLQueryItem(name: "type", value: "track"),
+            URLQueryItem(name: "limit", value: "5")
+        ]
+        
+        let url = searchEndpoint.appending(queryItems: queryItems)
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        print(try JSONSerialization.jsonObject(with: data))
+        
+        let response = try JSONDecoder().decode(
+            SpotifyApiSearchResponse.self,
+            from: data
+        )
+        
+        return response
+    }
+}
+
+@available(macOS 13.0, *)
+@available(iOS 16.0, *)
+public extension SpotifySearchApi {
+    
+    static var searchEndpoint: URL {
+        var components = URLComponents()
+        
+        components.scheme = "https"
+        components.host = "api.spotify.com"
+        components.path = "/v1/search"
+        
+        return components.url!
+    }
 }
