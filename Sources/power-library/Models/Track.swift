d705699a31ebfac612e9aba938e14a875e387802
HSamiul <samiulh@andrew.cmu.edu>
2024-10-01 22:44:54 -0400

    add mock instance of Track

--- Sources/power-library/Models/Track.swift
+++ Sources/power-library/Models/Track.swift
@@ -15,3 +15,12 @@ public struct Track: Hashable {
     
     public let imageUrl: URL
 }
+
+public extension Track {
+    
+    static var mock = Track(
+        trackName: "Fine China",
+        artistName: "Juice WRLD",
+        imageUrl: URL(string: "https://i.scdn.co/image/ab67616d0000b2738b27d35aa2c6dcf99895d664")!
+    )
+}
